da62696c711f37f1f87450694616263676713bbc
lehuy95 <huylevp95@gmail.com>
2020-08-26 14:52:19 +0700

    finish modeling-user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
 	root 'demo_apps#home'
 	get  '/help', to: 'demo_apps#help'
 	get  '/about', to: 'demo_apps#about'
 	get  '/contact', to: 'demo_apps#contact'
+	
+
+	get '/signup', to: 'users#new'
 	
 end
